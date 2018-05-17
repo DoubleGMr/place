a543d8bbdc3ceb641c19b4337004fdce7454278e
Jasonly <yao1508728331@gmail.com>
2018-05-17 13:13:43 +0800

    level 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+		account: "admin",
+		password: "admin123456"
+	)
+
+Tag.create!(
+		name: "粤东"
+	)
+
+Tag.create!(
+		name: "粤西"
+	)
